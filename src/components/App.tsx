eda708fe0674fea5716f2963089a4069ab5c2c57
Aryan Deora <adeora@iu.edu>
2021-12-25 12:09:02 -0500

    First Observation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { css } from '@emotion/css';
 
+// So looks like the component renders first, then the previous effect cleanup, then the next effect
 export const App: React.FC = () => {
+  const [count, setCount] = useState(0);
+  console.log(`rendering App ${count}`);
+
+  useEffect(() => {
+    console.log(`running Effect ${count}`);
+    return () => {
+      console.log(`cleaning up Effect ${count}`);
+    };
+  });
+
   return (
-    <div>
-      <h1>Hello World YO</h1>
+    <div className={appStyle}>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
     </div>
   );
 };
+
+const appStyle = css`
+  font-family: sans-serif;
+  border: 2px solid pink;
+`;
