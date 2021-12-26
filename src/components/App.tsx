efd5f7862b7bafeb77607cd55f08efcaccbf55c5
Aryan Deora <adeora@iu.edu>
2021-12-26 11:08:21 -0500

    Custom Hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { css } from '@emotion/css';
 
-// So looks like the component renders first, then the previous effect cleanup, then the next effect
+const useWindowsWidth = () => {
+  const [isScreenSmall, setIsScreenSmall] = useState(window.innerWidth < 600);
+  console.log('Running useWindowsWidth');
+  const checkScreenSize = () => {
+    console.log('Running checkScreenSize');
+    setIsScreenSmall(window.innerWidth < 600);
+  };
+  useEffect(() => {
+    checkScreenSize();
+    window.addEventListener('resize', checkScreenSize);
+
+    return () => window.removeEventListener('resize', checkScreenSize);
+  }, []);
+
+  return isScreenSmall;
+};
+
 export const App: React.FC = () => {
   const [count, setCount] = useState(0);
+  const isScreenSmall = useWindowsWidth();
+
   console.log(`rendering App ${count}`);
 
   useEffect(() => {
@@ -13,11 +31,14 @@ export const App: React.FC = () => {
     };
   });
 
+  console.log(`isScreenSmall: ${isScreenSmall}`);
   return (
     <div className={appStyle}>
       <h1>Count: {count}</h1>
       <button onClick={() => setCount(count + 1)}>+</button>
       <button onClick={() => setCount(count - 1)}>-</button>
+
+      <p>Width: {isScreenSmall ? 'true' : 'false'}</p>
     </div>
   );
 };
